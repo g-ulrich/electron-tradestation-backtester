428fab86a91f96b42b99c4e96ddc261f583c92f1
Gabe Ulrich Laptop <gabriel.ulrich@aol.com>
2024-12-30 10:44:24 -0500

    function update for strategies

--- src/renderer/index.js
+++ src/renderer/index.js
@@ -26,12 +26,15 @@ class Main {
      
     }
 
-    safeExecute(code) {
+    safeExecuteTest(code) {
         try {
-            return eval(code);
+            const buy = "buy";
+            const sell = "sell";
+            const hold = "hold";
+            eval(code);
+            return {success: true, error: ""};
         } catch (error) {
-            console.error("Error executing code:", error);
-            return null;
+            return {success: false, error: error};
         }
     }
 
@@ -42,12 +45,35 @@ class Main {
             var formData = {};
             $('#stockForm').find('*[id]').each(function() {
                 var id = $(this).attr('id');
-                formData[id] = $(`#${id}`).val();
+                if (id == "strategyCode"){
+                    formData[id] = glb_codeEditor.getValue();
+                } else {
+                    formData[id] = $(`#${id}`).val();
+                }
+                
             });
-            formData['code'] = glb_codeEditor.getValue();
             console.log(formData);
             self.safeExecute(formData?.code);
             self.formData = formData
+        });
+
+        $("#testCode").on('click', ()=>{
+            const btnFunc = (passObj) =>{
+                $("#testCode").empty();
+                $("#testCode").append(`<i class="fa-solid fa-vial ${!passObj ? 'fa-flip' : ''}"></i> ${!passObj ? 'Testing...' : passObj.success ? 'Success' : 'Failure'}`);
+                $("#codeErr").text(passObj && passObj.error?.message ? passObj.error?.message : ''); 
+                setTimeout(()=>{
+                    $("#testCode").empty();
+                    $("#testCode").append(`<i class="fa-solid fa-vial"></i> Test`);
+                    $("#codeErr").text(''); 
+                }, 10000);
+            }
+            btnFunc();
+            var code = glb_codeEditor.getValue();
+            var pass = self.safeExecuteTest(code);
+            setTimeout(()=>{
+                btnFunc(pass);
+            }, 700);
         });
     }
 
